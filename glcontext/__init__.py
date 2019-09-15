3665232b96a6652f1fa20dafa8ccd8d4b77ad41f
Szabolcs Dombi <cprogrammer1994@gmail.com>
2019-09-15 12:54:26 +0300

    shortcut for default backend

---
+++ glcontext/__init__.py
@@ -0,0 +1,27 @@
+def default_backend(standalone=False):
+    PLATFORMS = {'windows', 'linux', 'darwin'}
+
+    import platform
+    target = platform.system().lower()
+
+    for known in PLATFORMS:
+        if target.startswith(known):
+            target = known
+
+    if target not in PLATFORMS:
+        target = 'linux'
+
+    if target == 'windows':
+        from glcontext import wgl
+        mode = 'standalone' if standalone else 'detect'
+        return lambda glversion: wgl.create_context(mode=mode, glversion=glversion)
+
+    if target == 'linux':
+        from glcontext import x11
+        mode = 'standalone' if standalone else 'detect'
+        return lambda glversion: wgl.create_context(mode=mode, glversion=glversion)
+
+    if target == 'darwin':
+        from glcontext import darwin
+        mode = 'standalone' if standalone else 'detect'
+        return lambda glversion: wgl.create_context(mode=mode, glversion=glversion)
