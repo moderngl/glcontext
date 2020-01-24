7b9b34d328ac3325d6c85e2952a8beb38ef53a74
Einar Forselv <eforselv@gmail.com>
2020-01-24 19:33:24 +0100

    get_default_backend docstrings

--- glcontext/__init__.py
+++ glcontext/__init__.py
@@ -2,6 +2,22 @@ __version__ = '1.0.1'
 
 
 def default_backend(standalone=False):
+    """Get default backend based on the detected platform.
+    Supports detecting an existing context and standalone contexts.
+    If no context if found for the platform we return the linux backend.
+
+    Example::
+
+        # Get the available backend
+        backend = get_default_backend(standalone=False/True)
+        # Create an opengl 3.3 context or detect the currently active
+        # context requiring at least opengl 3.3 support.
+        ctx = backend(330)
+
+    Args:
+        standalone (bool): If ``False`` we detect an existing context.
+                           If ``True`` we create a headless/standalone context.
+    """
     PLATFORMS = {'windows', 'linux', 'darwin'}
 
     import platform
