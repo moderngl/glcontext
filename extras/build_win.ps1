854d5f34c88ed9f6337c18bee7d930cf57cbd985
Einar Forselv <eforselv@gmail.com>
2020-03-22 11:23:40 +0100

    Update build_win.ps1

--- extras/build_win.ps1
+++ extras/build_win.ps1
@@ -23,23 +23,23 @@ pip install -U setuptools wheel
 python setup.py bdist_wheel
 
 # 3.7
-pyenv global 3.7.5
+pyenv global 3.7.6
 pyenv rehash
 pip install -U setuptools wheel
 python setup.py bdist_wheel
 
-pyenv global 3.7.5-amd64
+pyenv global 3.7.6-amd64
 pyenv rehash
 pip install -U setuptools wheel
 python setup.py bdist_wheel
 
 # 3.8
-pyenv global 3.8.0
+pyenv global 3.8.1
 pyenv rehash
 pip install -U setuptools wheel
 python setup.py bdist_wheel
 
-pyenv global 3.8.0-amd64
+pyenv global 3.8.1-amd64
 pyenv rehash
 pip install -U setuptools wheel
 python setup.py bdist_wheel
