4c50140bc629fe3aa8d74b497e45ddc981ebd396
einarf <eforselv@gmail.com>
2021-02-13 15:01:52 +0100

    Bump version

--- glcontext/__init__.py
+++ glcontext/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '2.3'
+__version__ = '2.3.1'
 
 
 def default_backend():
--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ ext_modules = {
 
 setup(
     name='glcontext',
-    version='2.3',
+    version='2.3.1',
     description='Portable OpenGL Context',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
