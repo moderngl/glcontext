215a638e78242607a54df9008a10dd48f24bb76f
Szabolcs Dombi <cprogrammer1994@gmail.com>
2023-10-14 11:17:57 +0300

    prevent distutils not found exception

--- setup.py
+++ setup.py
@@ -15,13 +15,15 @@ if target not in PLATFORMS:
 
 if target == 'darwin':
     import os
-    from distutils.sysconfig import get_config_var
-    from distutils.version import LooseVersion
-    if 'MACOSX_DEPLOYMENT_TARGET' not in os.environ:
-        current_system = LooseVersion(platform.mac_ver()[0])
-        python_target = LooseVersion(get_config_var('MACOSX_DEPLOYMENT_TARGET'))
-        if python_target < '10.9' and current_system >= '10.9':
-            os.environ['MACOSX_DEPLOYMENT_TARGET'] = '10.9'
+
+    if sys.version_info[:2] < (3, 12):
+        from distutils.sysconfig import get_config_var
+        from distutils.version import LooseVersion
+        if 'MACOSX_DEPLOYMENT_TARGET' not in os.environ:
+            current_system = LooseVersion(platform.mac_ver()[0])
+            python_target = LooseVersion(get_config_var('MACOSX_DEPLOYMENT_TARGET'))
+            if python_target < '10.9' and current_system >= '10.9':
+                os.environ['MACOSX_DEPLOYMENT_TARGET'] = '10.9'
 
 wgl = Extension(
     name='glcontext.wgl',
